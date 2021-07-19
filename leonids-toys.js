1a4d863ca2f806199b12f0585b6b1c0f3676955c
Matthew Singler <matthew.singler@gmail.com>
2021-07-19 12:00:07 -0500

    Initial toys commit

---
+++ leonids-toys.js
@@ -0,0 +1,84 @@
+const toys = [
+{   id: 1,
+    name: "Rustler",
+    maker: "Traxxas",
+    ageUse: 6,
+    price: 300.00,
+    size: "medium"
+},
+
+{   id: 2,
+    name: "Star Flyer",
+    maker: "Shooter",
+    ageUse: 6,
+    price: 25.00,
+    size: "medium"
+},
+
+ {  id: 3,
+    name: "First Set",
+    maker: "Ludwig",
+    ageUse: 4,
+    price: 225.00,
+    size: "large"
+}
+]
+const kareoke = {
+    id: 4,
+    name: "Live Kareoke",
+    maker: "Blamo",
+    ageUse: 4,
+    price: 250.00,
+    size: "medium"
+}
+
+const log = {
+    id: 5,
+    name: "The Log",
+    maker: "Acme",
+    ageUse: 2,
+    price: 10.00,
+    size: "small"
+}
+toys.push(kareoke);
+toys.push(log);
+/*for (const toy of toys) {
+    console.log(toy.name)
+}
+for (const toy of toys) {
+    console.log(`The ${toy.name} costs ${toy.price} dollars.`)
+    
+}
+for (const toy of toys) {
+    toy.price = toy.price * .05 + toy.price;
+    console.log(`The new price of the ${toy.name} is ${toy.price} dollars.`)
+}*/
+const toyToFind = 5
+
+for (const toy of toys) {
+    if (toy.id === toyToFind) {
+        toy.price = toy.price * .05 + toy.price;
+        console.log(`The ${toy.name} is made by ${toy.maker} and costs ${toy.price} dollars.`)
+    }
+    
+}
+const powderedToastMan = {
+        name: "Ren and Stimpy",
+        maker: "Blamo",
+        ageUse: 13,
+        price: 10.00,
+        size: "Large"
+}
+const addToyToInventory = (toyObject) => {
+    const lastIndex = toys.length - 1
+    const currentLastToy = toys[lastIndex]
+    const maxId = currentLastToy.id
+    const idForNewToy = maxId + 1
+    toyObject.id = idForNewToy
+    toys.push(toyObject)
+}
+addToyToInventory(powderedToastMan)
+
+for (const toy of toys) {
+    console.log(`The ${toy.name} is made by ${toy.maker} and costs ${toy.price} dollars.`)
+}
